07c72e7ba656742fe5b009530ad762d1fa8290b4
Jonasz989 <76077510+Jonasz989@users.noreply.github.com>
2022-05-30 22:32:36 +0200

    Changing to gradle

--- src/Menu.java
+++ src/Menu.java
@@ -3,50 +3,36 @@ This program let us simulate a career of young Pokemon trainer
 In menu we can choose 1. Start simulation, 2. Setting 3. Exit*
  */
 import java.util.Scanner;
-
 public class Menu {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int choiceMenu=0;
+        int choiceMenu;
         Settings settings = new Settings();
-        while (true) {
-            while(true){
-                System.out.println("1. Start simulation");
-                System.out.println("2. Show current settings");
-                System.out.println("3. Change existing settings");
-                System.out.println("4. Exit");
-                boolean test = scanner.hasNextInt();
-                if(test){
-                    choiceMenu = scanner.nextInt();
+        while(true) {
+            System.out.println("1. Start simulation");
+            System.out.println("2. Show current settings");
+            System.out.println("3. Change existing settings");
+            System.out.println("4. Exit");
+            choiceMenu = scanner.nextInt();
+            switch (choiceMenu) {
+                case 1:
+                    Simulation essa = new Simulation();
+                    essa.start(essa.flatMap);
                     break;
-                }
-                else{
-                    System.out.println("You've entered a String variable instead of an Integer. Please try again.");
-                    System.out.println(" ");
-                }
-                scanner.nextLine();
-
+                case 2:
+                    settings.printSettings();
+                    break;
+                case 3:
+                    settings.changeSettings();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Your input was invalid. Please choose number 1-4.");
+                    break;
 
             }
-                switch (choiceMenu) {
-                    case 1:
-                        Simulation essa = new Simulation();
-                        essa.start(essa.flatMap);
-                        break;
-                    case 2:
-                        settings.printSettings();
-                        break;
-                    case 3:
-                        settings.changeSettings();
-                        break;
-                    case 4:
-                        System.exit(0);
-                    default:
-                        System.out.println("Your input was invalid. Please choose number 1-4.");
-                        break;
-
-                }
         }
     }
 }
