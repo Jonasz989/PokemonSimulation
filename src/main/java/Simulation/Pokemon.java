68c48fb6474d2107a2bcb6fd233e0f14c75cec13
Jonasz989 <76077510+Jonasz989@users.noreply.github.com>
2022-06-12 01:46:06 +0200

    działa

---
+++ src/main/java/Simulation/Pokemon.java
@@ -0,0 +1,31 @@
+package Simulation;
+
+public abstract class Pokemon {
+
+    int level;
+    static int CountOfPokemonsOnTheMap = -1;
+    Pokemon () {
+        level = 1;
+        CountOfPokemonsOnTheMap++;
+    }
+
+    Pokemon (int level) {
+        this.level = level;
+    }
+
+    //variable for Base Critical Hit Chance for the other pokemons, getter and setter for it as well
+    static float pokemonsCriticalHitBaseChance = 0;
+    float newPokemonsCriticalHitBaseChance=0;
+    public static float getPokemonsCriticalHitBaseChance() {
+        return pokemonsCriticalHitBaseChance;
+    }
+    public float setPokemonsCriticalHitBaseChance(float newPokemonsCriticalHitBaseChance) {
+        return pokemonsCriticalHitBaseChance = newPokemonsCriticalHitBaseChance;
+    }
+
+    //variable for the count of pokemons on the map, getter and setter for it as well
+    public static int getCountOfPokemonOnTheMap(){
+        return CountOfPokemonsOnTheMap;
+    }
+}
+
